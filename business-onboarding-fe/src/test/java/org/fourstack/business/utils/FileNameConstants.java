14e6e528f8b367f9d6eca840a5f7d3276f25d800
Manjunath HM <manjunathcs052@gmail.com>
2024-08-15 15:28:38 +0530

    Added test sample json files.

--- business-onboarding-fe/src/test/java/org/fourstack/business/utils/FileNameConstants.java
+++ business-onboarding-fe/src/test/java/org/fourstack/business/utils/FileNameConstants.java
@@ -5,4 +5,8 @@ public final class FileNameConstants {
     }
 
     public static final String BUSINESS_REGISTER_REQUEST = "json-files/create-business/business-register-request.json";
+    public static final String ADD_B2B_REQUEST = "json-files/b2b-requests/add-b2b-request.json";
+    public static final String EDIT_B2B_REQUEST = "json-files/b2b-requests/edit-b2bId-request.json";
+    public static final String CHECK_BUSINESS_REQUEST = "json-files/check-business/check-business-request.json";
+    public static final String SEARCH_BUSINESS_REQUEST = "json-files/check-business/search-business-request.json";
 }
