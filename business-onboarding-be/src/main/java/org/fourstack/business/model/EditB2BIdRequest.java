0f907157c727f48e06cde1b7740f3b11f6ccb155
Manjunath HM <manjunathcs052@gmail.com>
2024-08-17 12:43:12 +0530

    Added new pojo classes for Edit B2BId changes.

---
+++ business-onboarding-be/src/main/java/org/fourstack/business/model/EditB2BIdRequest.java
@@ -0,0 +1,17 @@
+package org.fourstack.business.model;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class EditB2BIdRequest implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 6164980099147900300L;
+    private CommonRequestData commonData;
+    private RequesterB2BId onboardingB2BIds;
+    private RegEditB2BIds regB2BIds;
+    private List<AdditionalInfo> additionalInfoList;
+}
