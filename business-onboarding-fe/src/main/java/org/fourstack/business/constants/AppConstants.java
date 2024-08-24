6bceb499546599eee57e969cefc21f984f25567d
Manjunath HM <38223055+ManjunathMGowda92@users.noreply.github.com>
2024-08-24 14:19:48 +0530

    Added kafka topic details for Activate / Deactivate B2B Id. (#11)
    * Updated the time stamp validations and test cases.
    
    * Added ReflectionTestUtils to resolve @Value resources at test runtime.
    
    * Added Kafka message audits for publisher service.
    
    * Added Edit B2B Id API changes.
    
    * Added changes for Activate Deactivate B2B ID API.
    
    * Updated changes Activate-Deactivate B2B ID.

--- business-onboarding-fe/src/main/java/org/fourstack/business/constants/AppConstants.java
+++ business-onboarding-fe/src/main/java/org/fourstack/business/constants/AppConstants.java
@@ -6,7 +6,7 @@ public final class AppConstants {
 
     public static final String PREFIX_MSG = "MSG";
     public static final String PREFIX_TXN = "TXN";
-    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error : Please contact administrator";
     public static final String ERROR_500 = "500";
     public static final String PAN_REGEX = "[A-Z]{5}[0-9]{4}[A-Z]{1}";
     public static final String PAN = "PAN";
